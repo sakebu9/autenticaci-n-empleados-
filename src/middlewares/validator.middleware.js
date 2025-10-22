1f6d339c44de7b529b503e4c646d1225b6c25885
sakebu9 <wmora185mail.com>
2025-10-21 21:31:55 -0500

    correccion datos vacios

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -5,13 +5,7 @@ export const validateSchema = (schema) => (req, res, next) => {
     schema.parse(req.body);
     next();
   } catch (error) {
-    if (error instanceof ZodError) {
-      return res.status(400).json({
-        error: error.issues.map((issue) => issue.message),
-      });
-    }
-
-    console.error("Unexpected validation error:", error);
-    return res.status(500).json({ error: ["Internal server error"] });
+    console.log(error);
+    return res.status(400).json({ error: error.issues.map(issue => issue.message) });
   }
 };
