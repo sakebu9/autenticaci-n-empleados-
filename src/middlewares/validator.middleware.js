52169d82dbda6ac7a7badf3c163ee1cdc8d627c1
sakebu9 <wmora185mail.com>
2025-10-21 22:40:50 -0500

    tasks

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -3,9 +3,17 @@ import { ZodError } from "zod";
 export const validateSchema = (schema) => (req, res, next) => {
   try {
     schema.parse(req.body);
-    next();
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ error: error.issues.map(issue => issue.message) });
+    return next();
+  } catch (err) {
+    if (!(err instanceof ZodError)) return res.status(400).json({ body: { _error: ["Invalid request"] } });
+
+    const issues = err.issues || [];
+    const errors = {};
+    issues.forEach((issue) => {
+      const key = issue.path?.[0] ?? "_";
+      errors[key] = errors[key] ? [...errors[key], issue.message] : [issue.message];
+    });
+
+    return res.status(400).json({ body: errors });
   }
 };
